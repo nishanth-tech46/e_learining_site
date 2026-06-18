aa62c0e9232b81ecef755d94080ad6b1eb25b756
nishanth-tech46 <kannannisha46@gmail.com>
2026-06-18 14:37:33 +0530

    Enable debug for troubleshooting

--- learnproject/settings.py
+++ learnproject/settings.py
@@ -291,7 +291,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Email settings for OTP
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
